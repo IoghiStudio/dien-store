b0a74aafbde4a14559c5d9f45ab89ab4f8a60d01
Nicu <ioghigithublab@gmail.com>
2022-08-15 01:12:09 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,27 +4,14 @@ import { Route , Link } from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
+import ShopPage from './pages/Shop/Shop';
 
-const Buci = () => (
-  <div>
-    <Link to='/buci2'>Buci2!!!!!</Link>
-    <h1>Nuci mere etc</h1>
-  </div>
-)
-
-const Buci2 = () => (
-  <div>
-    <Link to='/buci'>Buci</Link>
-    <h1>Nuci2 mere2 etc2</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
-      <Route path='/buci' component={Buci} />
-      <Route path='/buci2' component={Buci2} />
+      <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
