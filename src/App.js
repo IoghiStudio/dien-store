71b131a52d8ead75fc82e140514533bf73e99453
Nicu <ioghigithublab@gmail.com>
2022-08-19 03:20:01 -0700

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/Shop/Shop';
 import SignPage from './pages/SignPage/SignPage';
 import Header from './components/Header/Header';
-import { auth } from './firebase/firebase.utils';
+import { auth , createUserProfileDocument } from './firebase/firebase.utils';
+
 
 class App extends React.Component {
   constructor() {
@@ -20,9 +21,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
   
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          } , () => {
+            console.log(this.state);
+          })
+        });
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
     })
   }
 
