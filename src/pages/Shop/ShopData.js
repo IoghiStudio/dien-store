27e2f570f6bef670efdf862a84ae428c0a05f955
Nicu <ioghigithublab@gmail.com>
2022-08-22 06:35:51 -0700

    update

--- src/pages/Shop/ShopData.js
+++ src/pages/Shop/ShopData.js
@@ -66,49 +66,49 @@ const SHOP_DATA = [
     routeName: 'sneakers',
     items: [
       {
-        id: 1,
+        id: 10,
         name: 'Papuci caca',
         imageUrl: 'https://i.ebayimg.com/t/Cute-Poop-Emoji-Slippers-Plush-Cotton-Comfortable-Indoor-Bedroom-Shoe-One-Size-/00/s/NDIyWDUwMA==/z/sc4AAOSw0xRZpeG8/$_12.JPG',
         price: 220
       },
       {
-        id: 2,
+        id: 11,
         name: 'Adidas Yeezy',
         imageUrl: 'https://frankfurt.apollo.olxcdn.com/v1/files/0wn4m7i26cdz-RO/image;s=644x461',
         price: 280
       },
       {
-        id: 3,
+        id: 12,
         name: 'Black Converse',
         imageUrl: 'https://images.journeys.ca/images/products/1_212394_ZM_THERO.JPG',
         price: 110
       },
       {
-        id: 4,
+        id: 13,
         name: 'Nike White AirForce',
         imageUrl: 'https://www.epantofi.ro/media/catalog/product/cache/image/650x650/0/1/01_0000300934807_ki.jpg',
         price: 160
       },
       {
-        id: 5,
+        id: 14,
         name: 'Nike Red High Tops',
         imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
         price: 160
       },
       {
-        id: 6,
+        id: 15,
         name: 'Nike Brown High Tops',
         imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
         price: 160
       },
       {
-        id: 7,
+        id: 16,
         name: 'Air Jordan Limited',
         imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
         price: 190
       },
       {
-        id: 8,
+        id: 17,
         name: 'Timberlands',
         imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
         price: 200
@@ -121,31 +121,31 @@ const SHOP_DATA = [
     routeName: 'jackets',
     items: [
       {
-        id: 1,
+        id: 18,
         name: 'Black Jean Shearling',
         imageUrl: 'https://www.horsefeathers.eu/data/tmp/0/7/84887_0.jpg?1632389585_1',
         price: 125
       },
       {
-        id: 2,
+        id: 19,
         name: 'Blue Jean Jacket',
         imageUrl: 'https://www.horsefeathers.eu/data/tmp/0/7/84887_0.jpg?1632389585_1',
         price: 90
       },
       {
-        id: 3,
+        id: 20,
         name: 'Grey Jean Jacket',
         imageUrl: 'https://www.horsefeathers.eu/data/tmp/0/7/84887_0.jpg?1632389585_1',
         price: 90
       },
       {
-        id: 4,
+        id: 21,
         name: 'Brown Shearling',
         imageUrl: 'https://www.horsefeathers.eu/data/tmp/0/7/84887_0.jpg?1632389585_1',
         price: 165
       },
       {
-        id: 5,
+        id: 22,
         name: 'Tan Trench',
         imageUrl: 'https://www.horsefeathers.eu/data/tmp/0/7/84887_0.jpg?1632389585_1',
         price: 185
@@ -158,43 +158,43 @@ const SHOP_DATA = [
     routeName: 'womens',
     items: [
       {
-        id: 1,
+        id: 23,
         name: 'Blue Tanktop',
         imageUrl: 'https://image.shutterstock.com/image-vector/angry-gorilla-260nw-235447987.jpg',
         price: 25
       },
       {
-        id: 2,
+        id: 24,
         name: 'Floral Blouse',
         imageUrl: 'https://image.shutterstock.com/image-vector/angry-gorilla-260nw-235447987.jpg',
         price: 20
       },
       {
-        id: 3,
+        id: 25,
         name: 'Floral Dress',
         imageUrl: 'https://image.shutterstock.com/image-vector/angry-gorilla-260nw-235447987.jpg',
         price: 80
       },
       {
-        id: 4,
+        id: 26,
         name: 'Red Dots Dress',
         imageUrl: 'https://image.shutterstock.com/image-vector/angry-gorilla-260nw-235447987.jpg',
         price: 80
       },
       {
-        id: 5,
+        id: 27,
         name: 'Striped Sweater',
         imageUrl: 'https://image.shutterstock.com/image-vector/angry-gorilla-260nw-235447987.jpg',
         price: 45
       },
       {
-        id: 6,
+        id: 28,
         name: 'Yellow Track Suit',
         imageUrl: 'https://image.shutterstock.com/image-vector/angry-gorilla-260nw-235447987.jpg',
         price: 135
       },
       {
-        id: 7,
+        id: 29,
         name: 'White Blouse',
         imageUrl: 'https://image.shutterstock.com/image-vector/angry-gorilla-260nw-235447987.jpg',
         price: 20
@@ -207,37 +207,37 @@ const SHOP_DATA = [
     routeName: 'mens',
     items: [
       {
-        id: 1,
+        id: 30,
         name: 'Camo Down Vest',
         imageUrl: 'https://t4.ftcdn.net/jpg/01/92/36/05/360_F_192360523_dNz2SlzxEGy2Jwd3xIMzBvS13bRItKVN.jpg',
         price: 325
       },
       {
-        id: 2,
+        id: 31,
         name: 'Floral T-shirt',
         imageUrl: 'https://t4.ftcdn.net/jpg/01/92/36/05/360_F_192360523_dNz2SlzxEGy2Jwd3xIMzBvS13bRItKVN.jpg',
         price: 20
       },
       {
-        id: 3,
+        id: 32,
         name: 'Black & White Longsleeve',
         imageUrl: 'https://t4.ftcdn.net/jpg/01/92/36/05/360_F_192360523_dNz2SlzxEGy2Jwd3xIMzBvS13bRItKVN.jpg',
         price: 25
       },
       {
-        id: 4,
+        id: 33,
         name: 'Pink T-shirt',
         imageUrl: 'https://t4.ftcdn.net/jpg/01/92/36/05/360_F_192360523_dNz2SlzxEGy2Jwd3xIMzBvS13bRItKVN.jpg',
         price: 25
       },
       {
-        id: 5,
+        id: 34,
         name: 'Jean Long Sleeve',
         imageUrl: 'https://t4.ftcdn.net/jpg/01/92/36/05/360_F_192360523_dNz2SlzxEGy2Jwd3xIMzBvS13bRItKVN.jpg',
         price: 40
       },
       {
-        id: 6,
+        id: 35,
         name: 'Burgundy T-shirt',
         imageUrl: 'https://t4.ftcdn.net/jpg/01/92/36/05/360_F_192360523_dNz2SlzxEGy2Jwd3xIMzBvS13bRItKVN.jpg',
         price: 25
