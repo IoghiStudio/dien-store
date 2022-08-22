b4bac857d92e1f544eb0c191a09859f36c11da84
Nicu <ioghigithublab@gmail.com>
2022-08-22 12:58:51 -0700

    update

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -5,18 +5,22 @@ import './CartIcon.scss';
 import { connect } from "react-redux/es/exports";
 import { toggleCartHidden } from "../../redux/cart/cartActions";
 
-const CartIcon = ({ toggleCartHidden}) => (
+const CartIcon = ({ toggleCartHidden , itemCount}) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 )
+
+const mapStateToProps = ({ cart: { cartItems} }) => ({
+  itemCount: cartItems.reduce((acc , cartItem) => acc + cartItem.quantity , 0)
+})
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
   )(CartIcon);
