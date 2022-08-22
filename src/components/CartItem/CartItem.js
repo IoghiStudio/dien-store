a0c83acb00f68a7ab8f6b2b0b790081b7f74f2d2
Nicu <ioghigithublab@gmail.com>
2022-08-22 06:45:04 -0700

    update

---
+++ src/components/CartItem/CartItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './CartItem.scss';
+
+const CartItem = ({ item: {imageUrl , price , name , quantity} }) => (
+  <div className="card-item">
+    <img src={imageUrl} alt='item' />
+    <div className="item-details">
+      <span className="name">{name} </span>
+      <span className="price">{quantity} x ${price}</span>
+    </div>
+  </div>
+)
